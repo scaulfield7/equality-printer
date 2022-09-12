7eac22f30fb14a70b29000720e042d3fac20d9b2
scaulfield7 <scaulfield@live.ie>
2022-09-12 16:06:19 +0100

    Add flexibility with overloaded methods

--- src/EqualityPrinter.java
+++ src/EqualityPrinter.java
@@ -1,4 +1,28 @@
 public class EqualityPrinter{
+    public static void printEquality(int a, int b){
+        String result = a + ", " + b + ": ";
+        if(a < 0 || b < 0){
+            result = "Invalid value.";
+        }else if(a == b){
+            result += "Both numbers are equal.";
+        }else{
+            result += "Both numbers are different.";
+        }
+        System.out.println(result);
+    }
+    public static void printEquality(int a, int b, int c){
+        String result = a + ", " + b + ", " + c + ": ";
+        if(a < 0 || b < 0 || c < 0){
+            result = "Invalid value.";
+        }else if(a == b && b == c){
+            result += "All three numbers are equal.";
+        }else if(a != b && a != c && b != c){
+            result += "All three numbers are different.";
+        }else{
+            result += "These three numbers are neither all equal nor all different.";
+        }
+        System.out.println(result);
+    }
     public static void printEquality(int a, int b, int c, int d){
         String result = a + ", " + b + ", " + c + ", " + d + ": ";
         if(a < 0 || b < 0 || c < 0){
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main{
     public static void main(String[] args){
         EqualityPrinter equalityPrinter = new EqualityPrinter();
+        equalityPrinter.printEquality(349854, 22);
+        equalityPrinter.printEquality(762, 762);
+        equalityPrinter.printEquality(811, 812, 811);
+        equalityPrinter.printEquality(274, 274, 274);
+        equalityPrinter.printEquality(98, 40, 72984724);
         equalityPrinter.printEquality(4, 4, 4, 4);
         equalityPrinter.printEquality(32, 32, 32, 15);
         equalityPrinter.printEquality(-16, 7, 294, 43);
